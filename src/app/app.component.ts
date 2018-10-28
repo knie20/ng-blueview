202e379db596a2694d3a728fdb839eb5507b1f19
Greshbolt <greshbolt@gmail.com>
2018-10-27 20:36:15 -0400

    Added api requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { MatSort, MatTableDataSource } from '@angular/material';
 import { Component, ViewChild, OnInit } from '@angular/core';
-
-
+import { Http } from '@angular/http';
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -12,10 +12,15 @@ export class AppComponent{
   title = 'ng-blueview';
   displayedColumns: string[] =  ['time', 'state', 'city', 'zone'];
   dataSource = new MatTableDataSource(RADIO_DATA);
+  reportData;
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
-  }s
+  }
+  constructor(http:Http) {
+    this.reportData = http.get('api/people.json')
+      .pipe(map(response => response.json()));
+  }
   
 }
 export interface Transcript {
@@ -28,22 +33,5 @@ export interface Transcript {
 const RADIO_DATA: Transcript[] = [
   {time: '1', state: 'Ohio', city: 'Columbus', zone: '43201'},
   {time: '2', state: 'Ohio', city: 'Avon', zone: '44011'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
-  {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
   {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
 ];
