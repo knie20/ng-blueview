1e96d35130e47e944e02789409e194d842f7b915
Greshbolt <greshbolt@gmail.com>
2018-10-27 21:46:33 -0400

    Website looks nicer and now has buttons for a full transcript integration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,8 @@ import { MatSort, MatTableDataSource } from '@angular/material';
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { map } from "rxjs/operators";
+import {MatDialog} from '@angular/material';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent{
-  title = 'ng-blueview';
-  displayedColumns: string[] =  ['time', 'state', 'city', 'zone'];
-  dataSource = new MatTableDataSource(RADIO_DATA);
-  reportData;
-
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-  constructor(http:Http) {
-    this.reportData = http.get('api/people.json')
-      .pipe(map(response => response.json()));
-  }
-  
-}
 export interface Transcript {
   time: string;
   state: string;
@@ -35,3 +16,36 @@ const RADIO_DATA: Transcript[] = [
   {time: '2', state: 'Ohio', city: 'Avon', zone: '44011'},
   {time: '3', state: 'Ohio', city: 'Cincinatti', zone: '45219'},
 ];
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+  title = 'ng-blueview';
+  displayedColumns: string[] =  ['time', 'state', 'city', 'zone'];
+  dataSource = new MatTableDataSource(RADIO_DATA);
+  reportData;
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  /*constructor(http:Http) {
+    this.reportData = http.get('api/people.json')
+      .pipe(map(response => response.json()));
+  }*/
+  constructor(public dialog: MatDialog) {}
+  openDialog() {
+    const dialogRef = this.dialog.open(TableDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
+@Component({
+  selector: 'tabledialog',
+  templateUrl: 'tabledialog.html',
+})
+export class TableDialog {}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, TableDialog } from './app.component';
 
 
-import { MatFormFieldModule, MatInputModule, MatPaginatorModule, MatTableModule,MatIconModule, MatToolbarModule, MatTableDataSource, MatSortModule, MatSort } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatDialogModule, MatPaginatorModule, MatTableModule,MatIconModule, MatToolbarModule, MatTableDataSource, MatSortModule, MatSort } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TableDialog
      
   ],
   imports: [
@@ -21,9 +22,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     MatInputModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatDialogModule,
   ],
+  exports: [ TableDialog ],
   providers: [],
+  entryComponents: [ TableDialog],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
